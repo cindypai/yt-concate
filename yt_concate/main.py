43da9dd34fcaa4c2c84353264619e674356099e9
Cindy Pai <eln790901@gmail.com>
2021-06-26 17:47:40 +0800

    successfully got video urls from channel and add requirements

--- yt_concate/main.py
+++ yt_concate/main.py
@@ -2,16 +2,19 @@ import urllib.request as urlrq
 import json
 import certifi
 import ssl
+from yt_concate.settings import API_KEY
+print(API_KEY)
 
 CHANNEL_ID = 'UCKSVUHI9rbbkXhvAXK-2uxA'
 
+
 def get_all_video_in_channel(channel_id):
-    api_key = 'AIzaSyBV1k_HjnRA11kcnjtqEA-o1aNloXFKU08'
 
     base_video_url = 'https://www.youtube.com/watch?v='
     base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
 
-    first_url = base_search_url+'key={}&channelId={}&part=snippet,id&order=date&maxResults=25'.format(api_key, channel_id)
+    first_url = base_search_url + 'key={}&channelId={}&part=snippet,id&order=date&maxResults=25'.format(api_key,
+                                                                                                        channel_id)
 
     video_links = []
     url = first_url
@@ -26,10 +29,10 @@ def get_all_video_in_channel(channel_id):
         try:
             next_page_token = resp['nextPageToken']
             url = first_url + '&pageToken={}'.format(next_page_token)
-        except:
+        except KeyError:
             break
     return video_links
 
-
-video_list = get_all_video_in_channel(CHANNEL_ID)
-print(video_list)
+#
+# video_list = get_all_video_in_channel(CHANNEL_ID)
+# print(video_list)
